2f92aa95288bbd25fe4d56d1329037e5d0776647
Michael & Dan K <pair+MichaelDong32.danielkermode@devbootcamp.com>
2016-06-17 14:08:23 +1200

    Add summaries.json and /socks/:sock route

--- test/test.js
+++ test/test.js
@@ -4,7 +4,11 @@ var getPersonality = require('../lib/getPersonality')
 
 test('getPersonality returns three personality traits', function (t) {
   // action
-  var expected ={personality: 'Extraversion', needs: 'Self-expression', values: 'Hedonism'}
+  var expected = {
+    personality: 'Extraversion',
+    needs: 'Self-expression',
+    values: 'Hedonism'
+  }
   var result = getPersonality(testData)
   t.ok(result.personality)
   t.ok(result.needs)
